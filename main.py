087e6eb7bb98b940fa802a4211b6c15ca36082e4
harmonic259 <amin.shabani259@gmail.com>
2024-05-22 02:15:41 +0330

    base code

---
+++ main.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,9,0)
+objp = np.zeros((7 * 10, 3), np.float32)
+objp[:, :2] = np.mgrid[0:10, 0:7].T.reshape(-1, 2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
+images = glob.glob('imgs/*.jpg')
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (10, 7), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (10, 7), corners2, ret)
+        img = cv.resize(img, (500, 800))
+        cv.imshow('img', img)
+        cv.waitKey(500)
+
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+print("F:")
+print(mtx)
+print("Distortion:")
+print(dist)
+print("R:")
+print(rvecs)
+print("T:")
+print(tvecs)
+
+# Evaluating the result parameters
+mean_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2) / len(imgpoints2)
+    mean_error += error
+
+print("total error: ", mean_error / len(objpoints))
